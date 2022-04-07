2ce5d14393a4839f1be0669459678f5b392331bb
glebmatveev <glebmatveev@gmail.com>
2022-04-07 12:37:33 +0300

    Added POST method

--- public/index.php
+++ public/index.php
@@ -10,11 +10,14 @@ require_once __DIR__ . '/../vendor/autoload.php';
 
 $app = AppFactory::create();
 
+$app->addBodyParsingMiddleware();
+
 $app->addRoutingMiddleware();
 $app->add(new BasePathMiddleware($app));
 $app->addErrorMiddleware(true, true, true);
 
-$app->get('/', function (Request $request, Response $response) {
+// http://localhost:8888/customers-data/all
+$app->get('/customers-data/all', function (Request $request, Response $response) {
     $sql = "SELECT * FROM customers";
  
     try {
@@ -25,6 +28,43 @@ $app->get('/', function (Request $request, Response $response) {
       $db = null;
      
       $response->getBody()->write(json_encode($customers));
+      return $response
+        ->withHeader('content-type', 'application/json')
+        ->withStatus(200);
+    } catch (PDOException $e) {
+      $error = array(
+        "message" => $e->getMessage()
+      );
+   
+      $response->getBody()->write(json_encode($error));
+      return $response
+        ->withHeader('content-type', 'application/json')
+        ->withStatus(500);
+    }
+});
+
+// http://localhost:8888/customers-data/add
+$app->post('/customers-data/add', function (Request $request, Response $response, array $args) {
+    $data = $request->getParsedBody();
+    $name = $data["name"];
+    $email = $data["email"];
+    $phone = $data["phone"];
+   
+    $sql = "INSERT INTO customers (name, email, phone) VALUES (:name, :email, :phone)";
+   
+    try {
+      $db = new Db();
+      $conn = $db->connect();
+     
+      $stmt = $conn->prepare($sql);
+      $stmt->bindParam(':name', $name);
+      $stmt->bindParam(':email', $email);
+      $stmt->bindParam(':phone', $phone);
+   
+      $result = $stmt->execute();
+   
+      $db = null;
+      $response->getBody()->write(json_encode($result));
       return $response
         ->withHeader('content-type', 'application/json')
         ->withStatus(200);
