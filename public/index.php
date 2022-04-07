d2b98fa9752c16949cfc854c0b9724db6b3226ac
glebmatveev <glebmatveev@gmail.com>
2022-04-07 11:46:13 +0300

    Added DB settings and 1st JSON response

--- public/index.php
+++ public/index.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Models\DB;
 use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
 use Selective\BasePath\BasePathMiddleware;
@@ -14,8 +15,29 @@ $app->add(new BasePathMiddleware($app));
 $app->addErrorMiddleware(true, true, true);
 
 $app->get('/', function (Request $request, Response $response) {
-   $response->getBody()->write('Hello World!');
-   return $response;
+    $sql = "SELECT * FROM customers";
+ 
+    try {
+      $db = new DB();
+      $conn = $db->connect();
+      $stmt = $conn->query($sql);
+      $customers = $stmt->fetchAll(PDO::FETCH_OBJ);
+      $db = null;
+     
+      $response->getBody()->write(json_encode($customers));
+      return $response
+        ->withHeader('content-type', 'application/json')
+        ->withStatus(200);
+    } catch (PDOException $e) {
+      $error = array(
+        "message" => $e->getMessage()
+      );
+   
+      $response->getBody()->write(json_encode($error));
+      return $response
+        ->withHeader('content-type', 'application/json')
+        ->withStatus(500);
+    }
 });
 
 $app->run();
